4308e7c0b5ba09ce1ab80ff44476d5884801604c
TOS <TOS2022@users.noreply.github.com>
2022-11-07 23:17:06 +0200

    1 uzduotis padaryta galimai

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,29 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const converterForm = document.querySelector("form");
+
+converterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const kilograms = document.getElementById("search");
+  const pounds = kilogramsToPounds(kilograms.value);
+  const grams = kilogramsToGrams(kilograms.value);
+  const ounce = kilogramsToOunce(kilograms.value);
+
+  const output = document.getElementById("output");
+  output.textContent = `SVARAI:  ${pounds} [lb]; GRAMAI:  ${grams} [g]; UNCIJOS:  ${ounce} [oz];`;
+});
+
+function kilogramsToPounds(kilograms) {
+  return Number(kilograms) * 2.2046;
+}
+
+function kilogramsToGrams(kilograms) {
+  return Number(kilograms) / 0.001;
+}
+
+function kilogramsToOunce(kilograms) {
+  return Number(kilograms) * 35.274;
+}
