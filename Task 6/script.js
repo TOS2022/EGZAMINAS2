0b93a7e42d9225739c5422e0b05ed0cde312c7ee
TOS <TOS2022@users.noreply.github.com>
2022-11-08 02:27:47 +0200

    Users age

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,16 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAverageAge(usr) {
+  return usr.reduce((a, b) => a + b.age, 0) / usr.length;
+}
+
+console.log(Math.round(getUserAverageAge(users)));
+
+function getUsersNames(usr) {
+  const arr = usr.map((usr) => usr.name);
+  return arr;
+}
+
+console.log(getUsersNames(users));
