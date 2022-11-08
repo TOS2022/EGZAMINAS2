f018f9156e7a8545c9ecd30696ae4052d0c1c39d
TOS <TOS2022@users.noreply.github.com>
2022-11-08 02:28:42 +0200

    Filmo biudžetas

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,29 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+    }
+  
+    wasExpensive() {
+      if (this.budget >= 100000000) {
+        return console.log(
+          "Filmo biudžetas didesnis už > 100000000 USD",
+          true
+        );
+      } else {
+        return console.log(
+          "Filmo biudžetas mažesnis už < 100000000 USD",
+          false
+        );
+      }
+    }
+  }
+  
+  const series = new Movie("Mad Max: Fury Road", "George Miller", 185000000);
+  series.wasExpensive();
+  console.log("Filmo biudžetas: ", series.budget, "USD");
